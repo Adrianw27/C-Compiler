4bb967fac9cbe88f9641ece55d35bbed0e5d61ab
Adrian Winter <adrian.winter27@gmail.com>
2025-11-06 16:55:41 -0500

    asp multi variable support

--- src/AST.hpp
+++ src/AST.hpp
@@ -3,11 +3,12 @@
 #define AST_HPP
 
 #include <memory>
+#include <string>
 #include <vector>
 
 enum class AstKind {
     IntLiteral,
-    VarX,
+    Var,
     BinOp,
     Assign,
     ReturnStmt,
@@ -16,13 +17,22 @@ enum class AstKind {
 
 struct Ast {
     AstKind kind;
+
+    // IntLiteral
     int int_value = 0;
+
+    // Var / Assign
+    std::string var_name;
+
+    // BinOp
     char op = 0;
     std::unique_ptr<Ast> left;
     std::unique_ptr<Ast> right;
+
+    // Sequence of statements
     std::vector<std::unique_ptr<Ast>> stmts;
 
     Ast(AstKind k) : kind(k) {}
 };
 
-#endif 
+#endif
